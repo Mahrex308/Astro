c7bef63b19d30276bc438ad3f3e610ce8c877a62
Marino <35277555+Mahrex308@users.noreply.github.com>
2023-04-17 14:23:20 -0600

    part 5th tutorial

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+export async function get() {
+  return rss({
+    title: 'Astro Learner | Marino Olvera Blog',
+    description: 'My journey learning Astro in 2023',
+    site: 'teal-chimera-e4564d.netlify.app',
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>en-us</language>`,
+  });
+}
